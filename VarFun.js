32c6fa02f97f557fc63aede71240dda95f8e3116
amar <amarsljivo7@gmail.com>
2025-11-25 18:04:15 +0100

    Update VarFun.js

--- VarFun.js
+++ VarFun.js
@@ -1,3 +1,27 @@
+let vojnik1={ ime: "GIGN", godine: 30, gun: "MP5 hekler" }; 
+let vojnik2={ ime: "NAVY SEALS", godine: 29, gun: "M5 AR" }; 
+let vojnik3={ ime: "DELTA FRC", godine: 25, gun: "AR 15" }; 
+let vojnik4={ ime: "SAS", godine: 26, gun: "UZI" }; 
+
+let broj = 5;
+broj= 3+7*(3-2);
+console.log(broj);
+
+let string=`vojnik ${vojnik4} je iz Engleske`;
+console.log(string);
+
+let string1=`ja sam`;
+let string2=`sretan`;
+let string3=`tuzan`;
+
+let string4= string1+string2;
+console.log(string4);
+
+const konsttt = 8;
+console.log(konsttt);
+
+console.log(vojnik2);
+
 let trofejRonaldo = 40;
 let trofejMessi= 50;
 let trofejNeymar= 30;
@@ -22,4 +46,4 @@ let ucenik2 = { ime: "Mina", godine: 25, adresa: "dsds" };
 
 ucenik1.godine = 26;
 
-console.log(ucenik1);
+console.log(ucenik1)
